0c20cdc76e30f013a33c6d9b72c9c815fec27feb
MarioFLS <mariofernandolsantos@gmail.com>
2022-03-11 20:21:19 -0300

    Requesito 8 completo

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -6,12 +6,20 @@ import fetchToken from '../service/fetchToken';
 import Header from '../components/Header';
 import '../css/Game.css';
 
+const oneSecond = 1000;
 class Game extends Component {
-  state = { questions: {} };
+  state = {
+    questions: {},
+    answers: [],
+    timming: 30,
+    isDisabed: false,
+  };
 
   async componentDidMount() {
     await this.getQuestions();
     this.mapQuestions();
+    setInterval(this.timeToAwsers, oneSecond);
+    this.answersRandom();
   }
 
   async getQuestions() {
@@ -38,15 +46,30 @@ class Game extends Component {
     });
   }
 
-  /* referência para uso do sort(): https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array */
-  mapQuestions = () => {
+  answersRandom = () => {
     const { questions } = this.state;
     const { results } = questions;
-    if (results) {
+    const answers = results
+      .map(({ correct_answer: correctAnswer,
+        incorrect_answers: incorrectAnswers }) => [...incorrectAnswers, correctAnswer]
+        .sort(() => Math.random() - 1 / 2));
+    this.setState({ answers });
+  }
+
+  timeToAwsers = () => {
+    const { timming } = this.state;
+    if (timming > 0) return this.setState({ timming: timming - 1 });
+    return this.isDisabed();
+  }
+
+  /* referência para uso do sort(): https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array */
+  mapQuestions = () => {
+    const { questions, answers, isDisabed } = this.state;
+    const { results } = questions;
+    if (results && answers.length > 0) {
       return results
         .map((
-          { category, correct_answer: correctAnswer,
-            incorrect_answers: incorrectAnswers, question },
+          { category, correct_answer: correctAnswer, question },
           index,
         ) => {
           let indexWrongQuestions = 0;
@@ -55,21 +78,18 @@ class Game extends Component {
             indexWrongQuestions += 1;
             return indexWrongQuestions - 1;
           };
-
-          const erroQuestions = [...incorrectAnswers, correctAnswer]
-            .sort(() => ((Math.random() - (1 / 2))));
-
           return (
             <section key={ index }>
               <h4 data-testid="question-text">{question}</h4>
               <p data-testid="question-category">{category}</p>
               <div data-testid="answer-options">
                 {
-                  erroQuestions.map((questionsClick, indexQuestions) => (
+                  answers[0].map((questionsClick, indexQuestions) => (
                     <button
                       onClick={ () => this.checkAnswer(correctAnswer) }
                       className="answer-buttons"
                       key={ indexQuestions }
+                      disabled={ isDisabed }
                       data-testid={ questionsClick.includes(correctAnswer)
                         ? 'correct-answer' : `wrong-answer-${addIndex()}` }
                       type="button"
@@ -85,11 +105,20 @@ class Game extends Component {
     }
   }
 
+  isDisabed =() => {
+    const { timming } = this.state;
+    if (timming === 0) {
+      this.setState({ isDisabed: true });
+    }
+  }
+
   render() {
+    const { timming } = this.state;
     return (
       <div>
         <Header />
         {this.mapQuestions()}
+        {`Você tem: ${timming}s`}
       </div>
     );
   }
